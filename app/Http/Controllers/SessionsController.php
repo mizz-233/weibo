6805c40d04d64a6ac8f3ca46ced1718c2565c11c
mizz-233 <3409133605@qq.com>
2024-05-14 10:14:22 +0800

    创建会话控制器

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * @Author: mizz-233 3409133605@qq.com
+ * @Date: 2024-05-14 09:42:53
+ * @LastEditors: mizz-233 3409133605@qq.com
+ * @LastEditTime: 2024-05-14 10:12:33
+ * @FilePath: \weibo\app\Http\Controllers\SessionsController.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 09:47:29
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-13 14:59:40
+ * @LastEditTime: 2024-05-14 09:56:20
  * @FilePath: \weibo\routes\web.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,9 +11,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\SessionsController;
 
 Route::get('/', [StaticPagesController::class, 'home'])->name('home');
 Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
 Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
-Route::get('signup',[UsersController::class,'create'])->name('signup');
-Route::resource('users',UsersController::class);
+Route::get('signup', [UsersController::class, 'create'])->name('signup');
+Route::resource('users', UsersController::class);
+
+Route::get('login', [SessionsController::class, 'create'])->name('login');
+Route::post('login', [SessionsController::class, 'store'])->name('login');
+Route::delete('logout', [SessionsController::class, 'destroy'])->name('logout');
