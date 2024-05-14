44be49cfea5e399a5c83ca2608c5eb921c519ed7
mizz-233 <3409133605@qq.com>
2024-05-14 10:30:37 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-14 09:42:53
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-14 10:12:33
+ * @LastEditTime: 2024-05-14 10:26:15
  * @FilePath: \weibo\app\Http\Controllers\SessionsController.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,6 +12,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class SessionsController extends Controller
 {
@@ -21,17 +22,20 @@ class SessionsController extends Controller
     }
     public function store(Request $request)
     {
-        $credentials = $this->validate($request, [
-            'email' => 'required|email|max:255',
-            'password' => 'required'
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
         ]);
 
-        if (Auth::attempt($credentials)) {
-            session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
-        } else {
-            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back()->withInput();
-        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
