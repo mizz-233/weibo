395043f26910d72facf2db1fdd6c24f15173bd75
mizz-233 <3409133605@qq.com>
2024-05-14 11:45:06 +0800

    用户列表分页

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 15:20:08
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-14 11:30:26
+ * @LastEditTime: 2024-05-14 11:40:27
  * @FilePath: \weibo\app\Http\Controllers\UsersController.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -30,10 +30,10 @@ class UsersController extends Controller
             'only' => ['create']
         ]);
     }
-    
+
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(6);
         return view('users.index', compact('users'));
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * @Author: mizz-233 3409133605@qq.com
+ * @Date: 2024-05-13 09:47:29
+ * @LastEditors: mizz-233 3409133605@qq.com
+ * @LastEditTime: 2024-05-14 11:42:47
+ * @FilePath: \weibo\app\Providers\AppServiceProvider.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 namespace App\Providers;
 
@@ -23,6 +31,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Illuminate\Pagination\Paginator::useBootstrap();
     }
 }
