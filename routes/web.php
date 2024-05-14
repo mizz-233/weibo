6eb019fecc5e40b258ad9601417f698789b3b472
mizz-233 <3409133605@qq.com>
2024-05-14 15:01:31 +0800

    发送找回密码链接

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 09:47:29
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-14 09:56:20
+ * @LastEditTime: 2024-05-14 14:45:12
  * @FilePath: \weibo\routes\web.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\PasswordController;
 
 Route::get('/', [StaticPagesController::class, 'home'])->name('home');
 Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
@@ -23,4 +24,9 @@ Route::get('login', [SessionsController::class, 'create'])->name('login');
 Route::post('login', [SessionsController::class, 'store'])->name('login');
 Route::delete('logout', [SessionsController::class, 'destroy'])->name('logout');
 
-Route::get('signup/confirm/{token}', [UsersController::class,'confirmEmail'])->name('confirm_email');
+Route::get('signup/confirm/{token}', [UsersController::class, 'confirmEmail'])->name('confirm_email');
+
+Route::get('password/reset', [PasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [PasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [PasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset',[PasswordController::class,'reset'])->name('password.update');
