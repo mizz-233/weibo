abeb949a7ca95ca7f905092ea28920bb35459537
mizz-233 <3409133605@qq.com>
2024-05-14 17:17:13 +0800

    修复了上个版本bug，注册和重置密码的问题 版本回退到微博详情之前

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 15:20:08
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-14 14:21:09
+ * @LastEditTime: 2024-05-14 16:59:42
  * @FilePath: \weibo\app\Http\Controllers\UsersController.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -33,13 +33,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'summer@example.com';
-        $name = 'Summer';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
