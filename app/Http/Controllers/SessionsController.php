015c15b55f7aff82f37c7ef8e627196274faf9e7
mizz-233 <3409133605@qq.com>
2024-05-14 10:52:40 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-14 09:42:53
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-14 10:46:19
+ * @LastEditTime: 2024-05-14 10:50:51
  * @FilePath: \weibo\app\Http\Controllers\SessionsController.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,7 +27,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
@@ -42,3 +42,4 @@ class SessionsController extends Controller
         return redirect('login');
     }
 }
+
