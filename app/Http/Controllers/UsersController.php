af88c46599560c1421c9188be8d50eb6afd7fb63
mizz-233 <3409133605@qq.com>
2024-05-15 17:15:37 +0800

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 15:20:08
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-15 15:29:37
+ * @LastEditTime: 2024-05-15 17:13:22
  * @FilePath: \weibo\app\Http\Controllers\UsersController.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -127,5 +127,18 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
+    }
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = $user->name . '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = $user->name . '的粉丝';
+        return view('users.show_follow', compact('users', 'title'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 09:47:29
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-15 16:06:57
+ * @LastEditTime: 2024-05-15 17:10:20
  * @FilePath: \weibo\routes\web.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -33,3 +33,6 @@ Route::get('password/reset/{token}', [PasswordController::class, 'showResetForm'
 Route::post('password/reset', [PasswordController::class, 'reset'])->name('password.update');
 
 Route::resource('statuses', StatusesController::class, ['only' => ['store', 'destroy']]);
+
+Route::get('/users/{user}/followings', [UsersController::class,'followings'])->name('users.followings');
+Route::get('/users/{user}/followers', [UsersController::class,'followers'])->name('users.followers');
