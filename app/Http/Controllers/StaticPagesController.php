ccdd30475dcb6827c8b585790cfe59997fb0bf11
mizz-233 <3409133605@qq.com>
2024-05-15 16:22:27 +0800

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -1,9 +1,9 @@
 <?php
 /*
  * @Author: mizz-233 3409133605@qq.com
- * @Date: 2024-05-13 11:42:34
+ * @Date: 2024-05-13 14:10:05
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-13 13:37:50
+ * @LastEditTime: 2024-05-15 16:21:05
  * @FilePath: \weibo\app\Http\Controllers\StaticPagesController.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,17 +11,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
+
     public function help()
     {
         return view('static_pages/help');
     }
+
     public function about()
     {
         return view('static_pages/about');
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 09:47:29
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-15 15:28:38
+ * @LastEditTime: 2024-05-15 16:13:38
  * @FilePath: \weibo\app\Models\User.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -67,5 +67,10 @@ class User extends Authenticatable
     public function statuses()
     {
         return $this->hasMany(Status::class);
+    }
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
     }
 }
