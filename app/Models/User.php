c1dbb7ffe2a0e344caa3a7f5ffee32dc42cb4214
mizz-233 <3409133605@qq.com>
2024-05-15 17:29:23 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 09:47:29
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-15 16:59:32
+ * @LastEditTime: 2024-05-15 17:28:32
  * @FilePath: \weibo\app\Models\User.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -70,7 +70,10 @@ class User extends Authenticatable
     }
     public function feed()
     {
-        return $this->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
     public function followers()
