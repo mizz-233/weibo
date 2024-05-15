369c0b65447a4c0d00523bd517e0135864bd95f0
mizz-233 <3409133605@qq.com>
2024-05-15 16:12:08 +0800

    发布动态

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
  * @Author: mizz-233 3409133605@qq.com
  * @Date: 2024-05-13 09:47:29
  * @LastEditors: mizz-233 3409133605@qq.com
- * @LastEditTime: 2024-05-14 14:45:12
+ * @LastEditTime: 2024-05-15 16:06:57
  * @FilePath: \weibo\routes\web.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,6 +13,7 @@ use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PasswordController;
+use App\Http\Controllers\StatusesController;
 
 Route::get('/', [StaticPagesController::class, 'home'])->name('home');
 Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
@@ -29,4 +30,6 @@ Route::get('signup/confirm/{token}', [UsersController::class, 'confirmEmail'])->
 Route::get('password/reset', [PasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('password/email', [PasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 Route::get('password/reset/{token}', [PasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset',[PasswordController::class,'reset'])->name('password.update');
+Route::post('password/reset', [PasswordController::class, 'reset'])->name('password.update');
+
+Route::resource('statuses', StatusesController::class, ['only' => ['store', 'destroy']]);
